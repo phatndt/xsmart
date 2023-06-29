2f7f2efacf86ae6c920887990a51f5212ebdbaf5
phatndt <phatndt@ahamove.com>
2023-06-29 14:00:32 +0700

    [FEATURE] add unit converter scren

--- androidApp/src/main/java/my/phatndt/xsmart/android/features/unitconverter/presentation/view/UnitConverterScreen.kt
+++ androidApp/src/main/java/my/phatndt/xsmart/android/features/unitconverter/presentation/view/UnitConverterScreen.kt
@@ -1,15 +1,37 @@
 package com.phatndt.xsmart.features.unitconverter.presentation.view
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 //import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import my.phatndt.xsmart.android.features.currencyconverter.presentation.state.CurrencyUIState
+import my.phatndt.xsmart.android.features.currencyconverter.presentation.view.CurrencyConverterScreen
+import my.phatndt.xsmart.feature.currency.data.model.CurrencyRate
 
 @Composable
 fun UnitConverterScreen() {
     Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
         Text(text = "The feature is under development")
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Go back")
+        }
+    }
+}
+
+@Composable
+@Preview
+fun  UnitConverterScreenPreview() {
+    Scaffold() {
+        Box(modifier = Modifier.padding(it)) {
+            UnitConverterScreen()
+        }
     }
 }
